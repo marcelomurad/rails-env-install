bef779cf72e88297ef3fbab820e7ee1c55ecb8ab
Marcelo Mustafa Murad <marcelo.murad@dtmconsultoria.com>
2009-03-24 17:28:06 -0300

    First version of the script.

---
+++ rails-env-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Basic rails enviroment installation script for Ubuntu
+#Developed by Marcelo Murad - email at marcelomurad dot com
+#You can redistribute it and/or modify it under either the terms of the GPL (http://www.gnu.org/licenses/gpl-3.0.txt)
+
+sudo apt-get update
+
+sudo apt-get install -y ssh ruby ruby1.8-dev ruby-pkg-tools rdoc ri irb sqlite3 libopenssl-ruby libsqlite3-ruby sqlitebrowser
+
+sudo apt-get install wget
+
+mkdir temp_install_rails_env
+
+wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz -Otemp_install_rails_env/rubygems-1.3.1.tgz
+
+tar -xf temp_install_rails_env/rubygems-1.3.1.tgz
+
+cd rubygems-1.3.1
+
+sudo ruby setup.rb
+
+sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
+
+sudo gem install rails
