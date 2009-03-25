b818de90d17c9c25f73c98fb517f92627bb10d5e
Marcelo Mustafa Murad <marcelo.murad@dtmconsultoria.com>
2009-03-24 23:26:30 -0300

    Added ADDITIONAL_PACKAGES and ADDITIONAL_GEMS functionality.

--- rails-env-install.sh
+++ rails-env-install.sh
@@ -3,11 +3,17 @@
 #Developed by Marcelo Murad - email at marcelomurad dot com
 #You can redistribute it and/or modify it under either the terms of the GPL (http://www.gnu.org/licenses/gpl-3.0.txt)
 
+#separated by space
+ADDITIONAL_PACKAGES="mc"
+ADDITIONAL_GEMS="rspec"
+
 sudo apt-get update
 
 sudo apt-get install -y ssh ruby ruby1.8-dev ruby-pkg-tools rdoc ri irb sqlite3 libopenssl-ruby libsqlite3-ruby sqlitebrowser
 
-sudo apt-get install wget
+sudo apt-get install -y wget
+
+sudo apt-get install -y $ADDITIONAL_PACKAGES
 
 mkdir temp_install_rails_env
 
@@ -21,4 +27,6 @@ sudo ruby setup.rb
 
 sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
 
-sudo gem install rails
+sudo gem install rails -no-ri -no-rdoc
+
+sudo gem install $ADDITIONAL_GEMS
